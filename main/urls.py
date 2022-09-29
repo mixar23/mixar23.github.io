1c61dbdc2eb8e105e84390b6f66b6d8a10725dab
mixar23 <sasha.729@mail.ru>
2022-09-29 20:10:21 +0300

    twince_project

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.index)
+urlpatterns = [  
+    path('', views.index),
+    path('about-us', views.about)
 ]
--- main/views.py
+++ main/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    return render(request,"main/index.html")
+
+def about(request):
+    return render(request,"main/about.html")
