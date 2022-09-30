93cb29c250aceb7aee34bfa085714bce519e2a23
mixar23 <sasha.729@mail.ru>
2022-09-30 23:40:56 +0300

    first good try with img

--- main/views.py
+++ main/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def create_board():
+    board = []
+    for i in range(0,7):
+        for j in range(0,7):
+            board[i,j] = 0
+    return  board
+
+
 def index(request):
     return render(request,"main/index.html")
 
+
 def about(request):
-    return render(request,"main/about.html")
+    return render(request,"main/about.html", {"board":create_board})
