508736006cfc8ace8c65ea3cde8375bf7d0ff85f
mixar23 <sasha.729@mail.ru>
2022-10-11 12:05:35 +0300

    figure move logic in code need to apply

--- js/move.js
+++ js/move.js
@@ -8,9 +8,11 @@ function changepos(x,y,x1,y1,board_in){
     return board
     
 }
+//возможные ходы по пустому полю из точки с координатами x y фигурой fig 
 function can_move_to(x,y,fig){
     blocks = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
     blocks[x][y]  = 1;
+    //как ходят лдьи
     if (fig%6 == 4){
         for (let i = 0;i < 8;i++){
             for (let l = 0;l < 8;l++){
@@ -20,6 +22,7 @@ function can_move_to(x,y,fig){
             }
         }
     }
+    //как ходят кони
     if (fig%6 == 3){
             try{
                 blocks[x-2][y-1] = 1;
@@ -62,7 +65,7 @@ function can_move_to(x,y,fig){
                 console.log('-1_varxant');
             }        
             }
-
+    //как ходят слоны
     if  (fig%6 == 2){
             let x2 = x;
             let  y2 = y;
@@ -74,7 +77,7 @@ function can_move_to(x,y,fig){
             x2 = x;
             y2 = y;
             while ((x2 > 0)&&(y2 < 7)){
-                x2 +- 1;
+                x2 -= 1;
                 y2 += 1;
                 blocks[x2][y2] = 1;
             }
@@ -82,18 +85,99 @@ function can_move_to(x,y,fig){
             y2 = y;
             while ((x2 < 7)&&(y2 > 0)){
                 x2 += 1;
-                y2 +- 1;
+                y2 -= 1;
                 blocks[x2][y2] = 1;
             }
             x2 = x;
             y2 = y;
             while ((x2 > 0)&&(y2 > 0)){
-                x2 +- 1;
-                y2 +- 1;
+                x2 -= 1;
+                y2 -= 1;
                 blocks[x2][y2] = 1;
             }
         }
+        //как ходят ферзи
+        if  (fig%6 == 5){
+            for (let i = 0;i < 8;i++){
+                for (let l = 0;l < 8;l++){
+                    if ((i == x) || (l == y)){
+                        blocks[i][l] = 1;
+                    }
+                }
+            }
+            let x2 = x;
+            let  y2 = y;
+            while ((x2 < 7)&&(y2 < 7)){
+                x2 += 1;
+                y2 += 1;
+                blocks[x2][y2] = 1;
+            }
+            x2 = x;
+            y2 = y;
+            while ((x2 > 0)&&(y2 < 7)){
+                x2 -= 1;
+                y2 += 1;
+                blocks[x2][y2] = 1;
+            }
+            x2 = x;
+            y2 = y;
+            while ((x2 < 7)&&(y2 > 0)){
+                x2 += 1;
+                y2 -= 1;
+                blocks[x2][y2] = 1;
+            }
+            x2 = x;
+            y2 = y;
+            while ((x2 > 0)&&(y2 > 0)){
+                x2 -= 1;
+                y2 -= 1;
+                blocks[x2][y2] = 1;
+            }
 
+        }
+        if(fig%6 == 0){
+            for (let i = x - 1; i <= x + 1;i++){
+                for (let l = y - 1; l <= y + 1;l++){
+                    if ((7 >= l)&&(7 >= i)&&((l >= 0)&&(i >= 0))){
+                        blocks[i][l] = 1;
+                    }else{
+                        console.log('-1_variant');
+                    }
+                    }
+
+            }
+        }
+        if (fig == 1){
+            if (x != 7){
+                for (let i = y - 1;i <= y + 1;i++){
+                    if ((i >= 0)&&(i <= 7)){
+                        blocks[x+1][i] = 1;
+
+                    }
+                    
+                }
+            }
+            if (x == 1){
+                blocks[x+2][y] = 1;
+
+            }
+            
+        }
+        if (fig == 7){
+            if (x != 0){
+                for (let i = y - 1;i <= y + 1;i++){
+                    if ((i >= 0)&&(i <= 7)){
+                        blocks[x-1][i] = 1;
+
+                    }
+                    
+                }
+            }
+            if (x == 6){
+                blocks[x-2][y] = 1;
+                
+            }
+        }
     return blocks;
 
 
