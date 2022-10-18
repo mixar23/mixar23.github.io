a731b1e2d710beac4e1c8b2ac3b2c28f08f8abd7
mixar23 <sasha.729@mail.ru>
2022-10-18 13:38:18 +0300

    add beta test button

---
+++ js/reset.js
@@ -0,0 +1,40 @@
+// var sorted_ids = ['chess_figure_5_b','chess_figure_4_b','chess_figure_2_b','chess_figure_3_b','chess_figure_1_b','chess_figure_15_b','chess_figure_16_b','chess_figure_14_b','chess_figure_6_b','chess_figure_7_b','chess_figure_8_b','chess_figure_9_b','chess_figure_10_b','chess_figure_11_b','chess_figure_12_b','chess_figure_13_b','chess_figure_6','chess_figure_7','chess_figure_8','chess_figure_9','chess_figure_10','chess_figure_11','chess_figure_12','chess_figure_13','chess_figure_5','chess_figure_4','chess_figure_2','chess_figure_3','chess_figure_1','chess_figure_15','chess_figure_16','chess_figure_14'];
+
+// function Function_reset() {
+//     var top_s = 104;
+//     var left_s = 462;
+//     for(let i = 0; i < 8; i++){
+//         let element = document.getElementById(sorted_ids[i]);
+//         element.style.left = left_s  + 'px';
+//         element.style.top = top_s  +'px';
+//         left_s += 55;
+//     }
+//     left_s = 462;
+//     top_s += 55;
+//     for(let i = 8; i < 16; i++){
+//         let element = document.getElementById(sorted_ids[i]);
+//         element.style.left = left_s  + 'px';
+//         element.style.top = top_s  +'px';
+//         left_s += 55;
+//     }
+//     top_s = 433;
+//     left_s = 462;
+//     for(let i = 16; i < 24; i++){
+//         let element = document.getElementById(sorted_ids[i]);
+//         element.style.left = left_s  + 'px';
+//         element.style.top = top_s  +'px';
+//         left_s += 55;
+//     }
+//     top_s += 55;
+//     left_s = 462;
+//     for(let i = 24; i < 32; i++){
+//         let element = document.getElementById(sorted_ids[i]);
+//         element.style.left = left_s  + 'px';
+//         element.style.top = top_s  +'px';
+//         left_s += 55;
+//     }
+//     ybox = document.getElementById('cur_ste');
+//     ybox.style.left = 5000 + 'px';
+//     ybox2 = document.getElementById('cur_step2');
+//     ybox2.style.left = 5000 + 'px';
+// }
